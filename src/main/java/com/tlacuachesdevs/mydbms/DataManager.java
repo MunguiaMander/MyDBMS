ca97382ccb253ee44bc7d7a8c480670738ae259f
MunguiaMander <marcomunguiaalva@gmail.com>
2023-04-30 21:01:46 -0600

    Adding GUI and File Chooser

---
+++ src/main/java/com/tlacuachesdevs/mydbms/DataManager.java
@@ -0,0 +1,47 @@
+package com.tlacuachesdevs.mydbms;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author mander
+ */
+public class DataManager {
+
+    public void setBetsText(JButton uploadXMLButton, JTextArea xmlJTextArea) {
+
+        uploadXMLButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+
+                JFileChooser chooseData = new JFileChooser();
+                chooseData.showOpenDialog(null);
+                File data = chooseData.getSelectedFile();
+                try {
+                    FileReader readData = new FileReader(data);
+                    BufferedReader br = new BufferedReader(readData);
+                    String text = "";
+                    String line = "";
+                    while ((line = br.readLine()) != null) {
+                        text += line + "\n";
+                    }
+                    xmlJTextArea.setText(text);
+                    JOptionPane.showMessageDialog(null, "The file has been read");
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Error while tryng read the file");
+                }
+
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/tlacuachesdevs/mydbms/MyDBMS.java
@@ -0,0 +1,14 @@
+package com.tlacuachesdevs.mydbms;
+
+
+/**
+ *
+ * @author mander
+ */
+public class MyDBMS {
+
+    public static void main(String[] args) {
+        MainJFrame runMyDBMS = new MainJFrame();
+        runMyDBMS.setVisible(true);
+    }
+}
