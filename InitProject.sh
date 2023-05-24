f0984a1a25629911679dfdec31994cc3fe5fd51a
MunguiaMander <marcomugnuiaalva@gmail.com>
2023-05-24 10:52:48 -0600

    Add new lines to InitProject script from mander-lpt

--- InitProject.sh
+++ InitProject.sh
@@ -1,7 +1,10 @@
 #Marco Jose Munguia Alva 201931804
 
-#Instalamos lo necesario
+#Instalamos la  biblioteca de graficacion de diagramas ER
 sudo apt-get install graphviz
+
+#Instalamos maven que sera el encargado de crear nuestro .jar junto con sus dependencias
+sudo apt-get install maven
 
 #Compilamos con Maven para crear nuesto archivo .jar
 mvn clean package
@@ -11,3 +14,9 @@ mv target/MyDBMS-1.0-SNAPSHOT-jar-with-dependencies.jar ./
 
 #Iniciamos el programa
 java -jar MyDBMS-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+#Borramos el .jar y el ER creado para dejar limpio el proyecto
+sudo rm MyDBMS-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+sudo rm dbDiagram.png
+
